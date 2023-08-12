a6be793b0da160bb6147016fee2c1850bc5fc3a7
sumeet-08 <139367538+sumeet-08@users.noreply.github.com>
2023-08-12 16:30:10 +0530

    Update main.js

--- main.js
+++ main.js
@@ -13,21 +13,3 @@ club1.forEach((club1) => {
         club1.classList.toggle("active");
     });
 });
-
-const openPopupBtn = document.getElementById('openPopupBtn');
-const popupContainer = document.getElementById('popupContainer');
-const closePopupBtn = document.getElementById('closePopupBtn');
-
-openPopupBtn.addEventListener('click', () => {
-  popupContainer.style.display = 'block';
-});
-
-closePopupBtn.addEventListener('click', () => {
-  popupContainer.style.display = 'none';
-});
-
-popupContainer.addEventListener('click', (event) => {
-  if (event.target === popupContainer) {
-    popupContainer.style.display = 'none';
-  }
-});
